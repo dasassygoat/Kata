734fc1e6442698eed465bc02d17e5f94e2b7d644
Journeyman32 <bryan@coderbox.co>
2022-01-03 23:50:00 -0600

    Initial commit, first .net 6 project

---
+++ kata/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+public static class Kata
+{
+    public static int Score(int[] dice)
+    {
+        int threeOnes = 1000;
+        int threeTwos = 200;
+        int threeThrees = 300;
+        int threeFours = 400;
+        int threeFives = 500;
+        int threeSixes = 600;
+        int one = 100;
+        int five = 50;
+
+        int totalValue = 0;
+
+        int oneCount = 0;
+        int twoCount = 0; int threeCount = 0; int fourCount = 0; int fiveCount = 0; int sixCount = 0;
+
+        for (var x = 0; x < 5; x++)
+        {
+           
+        }
+
+        return 0;
+    }
+}
