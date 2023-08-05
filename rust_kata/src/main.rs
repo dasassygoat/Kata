63e8b3f1b2b1741504367ef1474b2b6e4e884a45
journeyman32 <bryan@coderbox.co>
2023-08-05 02:22:26 -0500

    rust kata checkin

--- rust_kata/src/main.rs
+++ rust_kata/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main(){
+    let sheeps = [true,  true,  true,  false,
+        true,  true,  true,  true ,
+        true,  false, true,  false,
+        true,  false, false, true ,
+        true,  true,  false,  true ,
+        false, false, true,  true];
+    let count = count_sheep(&sheeps);
+    println!("{count}");
+
+    let sum = summation(2);
+    eprintln!("{sum}");
 }
+
+fn count_sheep(sheep: &[bool]) -> u8 {
+    let mut count = 0;
+    for &s in sheep {
+        if s == true {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn summation(n: i32) -> i32 {
+    // Kata voted best practice was
+    // (1..=n).sum()
+    let mut sum =0;
+    let mut count = 1;
+    while count <= n {
+        sum += count;
+        count += 1;
+    }
+    return sum;
+}
+
